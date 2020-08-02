fd2c9103024990e94b2ad10111ac70f1cefcc322
Prashant Bhardwaj <scea.prashant@gmail.com>
2020-08-02 12:56:17 -0400

    updated cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_kJ64X1i1X',
+        userPoolClientId: '74b5l5blkpqav5tsf9djp58g8o',
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
